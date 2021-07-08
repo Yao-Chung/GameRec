11377f1f067787235b41d4835f82fb44d423d6c3
Chang Yao Chung <yaochung9744@gmail.com>
2021-07-08 10:59:33 +0800

    SECOND TEST

--- src/main/java/com/GameRec/GameRecSystem/database/DynamodbResource.java
+++ src/main/java/com/GameRec/GameRecSystem/database/DynamodbResource.java
@@ -16,7 +16,7 @@ public class DynamodbResource {
     public List<Messages> getAllValues() {
         List<Messages> datas = new ArrayList<>();
         datas = dbRespository.getInformation();
-        System.out.println("Database basic API");
+        System.out.println("HELLO FORK");
         return datas;
     }
 }
